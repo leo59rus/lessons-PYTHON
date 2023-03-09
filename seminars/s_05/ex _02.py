edfd6389100fa35e9b1ea85af0c3c666866246bd
leonid <orange59usa@gmail.com>
2023-03-09 20:48:29 +0300

    s05end

--- seminars/s_05/ex _02.py
+++ seminars/s_05/ex _02.py
@@ -1,1 +1,7 @@
-# 1.05
+# изменить максимальные оценки на отрицательные
+n = int(input("введите колво оценок: "))
+list = [int(input(f"введи {i + 1}-ю оценку: ")) for i in range(n)]
+x = min(list)
+y =max(list)
+list2= [x if i == y else i for i in list]
+print(list2)
