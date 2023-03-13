52e30bcc51cbf1d563e3077347fa6aa3ac9ec623
leonid <orange59usa@gmail.com>
2023-03-13 20:12:23 +0300

    s06

--- hw/hw5/ex_01.py
+++ hw/hw5/ex_01.py
@@ -12,7 +12,8 @@ b = int(input("b = "))
 def func(a, b):
     if b == 1:
         return a
-    if b != 1:
-        return (a * func(a, b - 1))
+    if b == 0 or a == 1 or a == 0:
+        return 1
+    return a * func(a, b - 1)
     
 print(func(a, b))
--- seminars/s_05/ex _02.py
+++ seminars/s_05/ex _02.py
@@ -2,6 +2,6 @@
 n = int(input("введите колво оценок: "))
 list = [int(input(f"введи {i + 1}-ю оценку: ")) for i in range(n)]
 x = min(list)
-y =max(list)
-list2= [x if i == y else i for i in list]
+y = max(list)
+list2 = [x if i == y else i for i in list]
 print(list2)
